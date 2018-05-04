d1b72ab48a6188fd53beec7c6e42df4368f54c81
martinperez2306 <perezmartincontacto@gmail.com>
2018-05-03 22:24:57 -0300

    Parte 3 del lab: implementacion de redirecciones y pipes

--- exec.c
+++ exec.c
@@ -32,7 +32,6 @@ static void get_environ_value(char* arg, char* value, int idx) {
 static void set_environ_vars(char** eargv, int eargc) {
 
 	// Your code here
-	int p;
 		for(int i = 0; i < eargc; i++){
 			char* varEnv = eargv[i];
 			int idx = block_contains(varEnv,'=');
@@ -42,7 +41,7 @@ static void set_environ_vars(char** eargv, int eargc) {
 				char* value = malloc(strlen(varEnv) - (idx + 1));
 				get_environ_key(varEnv,key);
 				get_environ_value(varEnv,value,idx);
-				int set = setenv(key,value,0);
+				int set = setenv(key,value,1);
 				if(set < 0){
 					perror("SetEnv");
 				}
@@ -73,14 +72,16 @@ static int open_redir_fd(char* file) {
 void execute(struct cmd* cmd){
 	//Cast struct cmd* to struct execcmd* to use this interface
     struct execcmd* execcmd = (struct execcmd*) cmd;
-
-    //Set Enviroment Variables
+   //Set Enviroment Variables
     set_environ_vars(execcmd->eargv, execcmd->eargc);
 	//Si le paso el execcmd->scmd me va a guardar el comando con parametros y todo
 	//Debo pasarle execcmd->argv[0]
 	//Example ls -l -a -> execcmd->scmd = "ls -l -a"; execcmd->argv = {"ls","-l","-a",(char*)NULL}
-	execvp(execcmd->argv[0],execcmd->argv);
+	int execError = execvp(execcmd->argv[0],execcmd->argv);
 	//En caso de que no haya programa que ejecutar salgo del proceso
+	if(execError < 0){
+		printf("Error al ejecutar el comando\n");
+	}
 	_exit(-1);
 }
 
@@ -91,6 +92,129 @@ void background(struct cmd* cmd){
 	struct backcmd* backcmd = (struct backcmd*) cmd;
 	//Call execute with struct cmd* contains in backcmd*
 	execute(backcmd->c);
+}
+
+void redirection(struct cmd* cmd){
+	//Cast struct cmd* to struct execcmd* to use this interface
+    struct execcmd* execcmd = (struct execcmd*) cmd;
+    //Initializing file descriptors
+    //Old FD
+    int ofd = 0;
+    int ifd = 0;
+    int efd = 0;
+    //New FD
+    int newIfd = 0;
+	int newOfd = 0;
+	int newEfd = 0;
+    if(strlen(execcmd->in_file) > 0){
+		if((ifd = open(execcmd->in_file,O_RDONLY)) < 0){
+			perror("Error in input file");
+		}
+    }
+    if(strlen(execcmd->out_file) > 0){
+    	//Condicion para redireccionar truncando archivos '>' o concatenando '>>'
+    	if(execcmd->out_file[0] == '>'){
+    		strcpy(execcmd->out_file, execcmd->out_file + 1);
+    		if((ofd = open(execcmd->out_file,O_CREAT|O_RDWR|O_APPEND,S_IRUSR|S_IWUSR)) < 0){
+    			perror("Error in output file");
+    		}
+    	}else{
+    		if((ofd = open(execcmd->out_file,O_CREAT|O_RDWR|O_TRUNC,S_IRUSR|S_IWUSR)) < 0){
+    			perror("Error in output file");
+    		}
+    	}	
+    }
+    if(strlen(execcmd->err_file) > 0){
+    	//Condicion para redireccionar el error estandar a la salida estandar '&>'
+    	printf("%s\n", execcmd->err_file);
+    	if(execcmd->err_file[0] == '&'){
+    		newEfd = dup2(STDOUT_FILENO,STDERR_FILENO);
+    	}
+    	else{
+    		if((efd = open(execcmd->err_file,O_CREAT|O_RDWR|O_TRUNC,S_IRUSR|S_IWUSR)) < 0){
+    			perror("Error in error file");
+    		}
+    	}
+    }
+	if(ifd < 0 || ofd < 0 || efd < 0){
+		close(ifd);
+		close(ofd);
+		close(efd);
+		_exit(-1);		
+	}else{
+		if(ofd > 0){
+			if(newEfd > 0){
+				newOfd = dup2(ofd,newEfd);
+			}else{
+				newOfd = dup2(ofd,STDOUT_FILENO);	
+			}			
+			close(ofd);
+		}
+		if(ifd > 0){
+			newIfd = dup2(ifd,STDIN_FILENO);
+			close(ifd);
+		}
+		if(efd > 0){
+			newEfd = dup2(efd,STDERR_FILENO);
+			close(efd);
+		}		
+		if(newIfd < 0 || newOfd < 0 || newEfd < 0){
+			perror("Error");
+			_exit(-1);
+		}else{
+			execute(cmd);
+		}
+	}  
+}
+
+void run_pipe(struct cmd* cmd){
+
+	//Cast struct cmd* to struct pipecmd* to use this interface
+	struct pipecmd* pipecmd = (struct pipecmd*) cmd;
+	int pipefd[2];
+	int pipeStatus = 0;
+	pid_t pid = 0;
+	int status = 0;
+
+	//Hago un pipe para generar los dos file descriptors
+	pipeStatus = pipe(pipefd);
+
+	if(pipeStatus == -1){
+		perror("Error");
+	}
+
+	//Hago un fork para crear un proceso hijo el cual copia todo el contexto del padre
+	if((pid = fork()) == 0){
+		//Cierro el file descriptor que no usa el hijo (read end)
+		if(close(pipefd[0]) == -1){
+			perror("Error INPUT");
+		}
+		//El hijo va a ejecutar un programa redirigiendo lo que escribe en
+		//salida estandar a la parte de escritura del pipe
+		dup2(pipefd[1],STDOUT_FILENO);
+		//Cierro el file descriptor que uso el hijo (write end)
+		if(close(pipefd[1]) == -1){
+			perror("Error OUTPUT");
+		}
+		//Ejecuto el left del pipe
+		execute(pipecmd->leftcmd);
+	}else{
+		//espero a que termine el hijo
+		waitpid(pid,&status,0);
+		//Cierro el file descriptor que no usa el padre (write end)
+		if(close(pipefd[1]) == -1){
+			perror("Error OUTPUT");
+		}
+		//El padre va a ejecutar un programa redirigiendo lo que lee en
+		//entrada estandar a la parte de lectura del pipe
+		dup2(pipefd[0],STDIN_FILENO);
+		//Cierro el file descriptor que uso el padre (read end)
+		if(close(pipefd[0]) == -1){
+			perror("Error INPUT");
+		}
+		//Ejecuto el right del pipe
+		execute(pipecmd->rightcmd);
+	}
 }
 
 // executes a command - does not return
@@ -126,8 +250,9 @@ void exec_cmd(struct cmd* cmd) {
 			// changes the input/output/stderr flow
 			//
 			// Your code here
-			printf("Redirections are not yet implemented\n");
-			_exit(-1);
+			redirection(cmd);
+			//printf("Redirections are not yet implemented\n");
+			//_exit(-1);
 			break;
 		}
 		
@@ -135,8 +260,8 @@ void exec_cmd(struct cmd* cmd) {
 			// pipes two commands
 			//
 			// Your code here
-			printf("Pipes are not yet implemented\n");
-				
+			//printf("Pipes are not yet implemented\n");
+			run_pipe(cmd);	
 			// free the memory allocated
 			// for the pipe tree structure
 			free_command(parsed_pipe);
